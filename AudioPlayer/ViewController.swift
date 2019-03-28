f499fea34af2b2ca0fdeeaa7b64289fd3c99d925
Ricardo Hui <eehaha@gmail.com>
2019-03-29 07:00:48 +0800

    can pause and play song, adjust volume

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
+    @IBAction func pause(_ sender: Any) {
+        audioPlayer.pause()
+    }
+    @IBAction func play(_ sender: Any) {
+        audioPlayer.play()
+    }
+    @IBOutlet var slider: UISlider!
+    @IBAction func sliderMoved(_ sender: Any) {
+        audioPlayer.volume = slider.value
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let audioPath = Bundle.main.path(forResource: "mozart-piano", ofType: "mp3")
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            audioPlayer.play()
+        }catch{
+            
+        }
     }
 
 
